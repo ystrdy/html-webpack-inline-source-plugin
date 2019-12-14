df675febe764f4780c03d4cac1aced32fc45bf6f
xuxufei <xuxufei@2144.cn>
2019-12-14 18:35:54 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,137 +1,136 @@
-'use strict';
-var escapeRegex = require('escape-string-regexp');
-var path = require('path');
-var slash = require('slash');
-var sourceMapUrl = require('source-map-url');
-var pkg = require('./package.json');
+const escapeRegex = require('escape-string-regexp');
+const path = require('path');
+const slash = require('slash');
+const sourceMapUrl = require('source-map-url');
 
-function HtmlWebpackInlineSourcePlugin (htmlWebpackPlugin) {
-  this.htmlWebpackPlugin = htmlWebpackPlugin;
-}
+const PLUGIN_NAME = 'html-webpack-inline-source-plugin';
+
+function HtmlWebpackInlineSourcePlugin() {}
 
 HtmlWebpackInlineSourcePlugin.prototype.apply = function (compiler) {
-  // Hook into the html-webpack-plugin processing
-  compiler.hooks.compilation.tap(pkg.name, compilation => {
-    compilation
-      .hooks
-      .htmlWebpackPluginAlterAssetTags
-      .tapAsync(pkg.name, (htmlPluginData, callback) => {
-        if (!htmlPluginData.plugin.options.inlineSource) {
-          return callback(null, htmlPluginData);
-        }
+    // Hook into the html-webpack-plugin processing
+    compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
+        compilation
+            .hooks
+            .htmlWebpackPluginAlterAssetTags
+            .tapAsync(PLUGIN_NAME, (htmlPluginData, callback) => {
+                if (!htmlPluginData.plugin.options.inlineSource) {
+                    return callback(null, htmlPluginData);
+                }
 
-        var regexStr = htmlPluginData.plugin.options.inlineSource;
+                const regexStr = htmlPluginData.plugin.options.inlineSource;
 
-        var result = this.processTags(compilation, regexStr, htmlPluginData);
+                const result = this.processTags(compilation, regexStr, htmlPluginData);
 
-        callback(null, result);
-      });
-  });
+                callback(null, result);
+            });
+    });
 };
 
 HtmlWebpackInlineSourcePlugin.prototype.processTags = function (compilation, regexStr, pluginData) {
-  var head = [];
-  var body = [];
+    const body = [];
+    const head = [];
 
-  var regex = new RegExp(regexStr);
-  var filename = pluginData.plugin.options.filename;
+    const regex = new RegExp(regexStr);
+    const filename = pluginData.plugin.options.filename;
 
-  pluginData.head.forEach(function (tag) {
-    head.push(this.processTag(compilation, regex, tag, filename));
-  });
+    pluginData.head.forEach(tag => {
+        head.push(this.processTag(compilation, regex, tag, filename));
+    });
 
-  pluginData.body.forEach(function (tag) {
-    body.push(self.processTag(compilation, regex, tag, filename));
-  });
+    pluginData.body.forEach(tag => {
+        body.push(this.processTag(compilation, regex, tag, filename));
+    });
 
-  return { headTags: headTags, bodyTags: bodyTags, plugin: pluginData.plugin, outputName: pluginData.outputName };
+    return { head, body, plugin: pluginData.plugin, outputName: pluginData.outputName };
 };
 
 HtmlWebpackInlineSourcePlugin.prototype.resolveSourceMaps = function (compilation, assetName, asset) {
-  var source = asset.source();
-  var out = compilation.outputOptions;
-  // Get asset file absolute path
-  var assetPath = path.join(out.path, assetName);
-  // Extract original sourcemap URL from source string
-  if (typeof source !== 'string') {
-    source = source.toString();
-  }
-  var mapUrlOriginal = sourceMapUrl.getFrom(source);
-  // Return unmodified source if map is unspecified, URL-encoded, or already relative to site root
-  if (!mapUrlOriginal || mapUrlOriginal.indexOf('data:') === 0 || mapUrlOriginal.indexOf('/') === 0) {
-    return source;
-  }
-  // Figure out sourcemap file path *relative to the asset file path*
-  var assetDir = path.dirname(assetPath);
-  var mapPath = path.join(assetDir, mapUrlOriginal);
-  var mapPathRelative = path.relative(out.path, mapPath);
-  // Starting with Node 6, `path` module throws on `undefined`
-  var publicPath = out.publicPath || '';
-  // Prepend Webpack public URL path to source map relative path
-  // Calling `slash` converts Windows backslashes to forward slashes
-  var mapUrlCorrected = slash(path.join(publicPath, mapPathRelative));
-  // Regex: exact original sourcemap URL, possibly '*/' (for CSS), then EOF, ignoring whitespace
-  var regex = new RegExp(escapeRegex(mapUrlOriginal) + '(\\s*(?:\\*/)?\\s*$)');
-  // Replace sourcemap URL and (if necessary) preserve closing '*/' and whitespace
-  return source.replace(regex, function (match, group) {
-    return mapUrlCorrected + group;
-  });
+    let source = asset.source();
+    const out = compilation.outputOptions;
+    // Get asset file absolute path
+    const assetPath = path.join(out.path, assetName);
+    // Extract original sourcemap URL from source string
+    if (typeof source !== 'string') {
+        source = source.toString();
+    }
+    const mapUrlOriginal = sourceMapUrl.getFrom(source);
+    // Return unmodified source if map is unspecified, URL-encoded, or already relative to site root
+    if (!mapUrlOriginal || mapUrlOriginal.indexOf('data:') === 0 || mapUrlOriginal.indexOf('/') === 0) {
+        return source;
+    }
+    // Figure out sourcemap file path *relative to the asset file path*
+    const assetDir = path.dirname(assetPath);
+    const mapPath = path.join(assetDir, mapUrlOriginal);
+    const mapPathRelative = path.relative(out.path, mapPath);
+    // Starting with Node 6, `path` module throws on `undefined`
+    const publicPath = out.publicPath || '';
+    // Prepend Webpack public URL path to source map relative path
+    // Calling `slash` converts Windows backslashes to forward slashes
+    const mapUrlCorrected = slash(path.join(publicPath, mapPathRelative));
+    // Regex: exact original sourcemap URL, possibly '*/' (for CSS), then EOF, ignoring whitespace
+    const regex = new RegExp(escapeRegex(mapUrlOriginal) + '(\\s*(?:\\*/)?\\s*$)');
+    // Replace sourcemap URL and (if necessary) preserve closing '*/' and whitespace
+    return source.replace(regex, function (match, group) {
+        return mapUrlCorrected + group;
+    });
 };
 
 HtmlWebpackInlineSourcePlugin.prototype.processTag = function (compilation, regex, tag, filename) {
-  var assetUrl;
+    let assetUrl;
 
-  // inline js
-  if (tag.tagName === 'script' && tag.attributes && regex.test(tag.attributes.src)) {
-    assetUrl = tag.attributes.src;
-    tag = {
-      tagName: 'script',
-      closeTag: true,
-      attributes: {
-        type: 'text/javascript'
-      }
-    };
+    // inline js
+    if (tag.tagName === 'script' && tag.attributes && regex.test(tag.attributes.src)) {
+        assetUrl = tag.attributes.src;
+        tag = {
+            tagName: 'script',
+            closeTag: true,
+            attributes: {
+                type: 'text/javascript'
+            }
+        };
 
-  // inline css
-  } else if (tag.tagName === 'link' && regex.test(tag.attributes.href)) {
-    assetUrl = tag.attributes.href;
-    tag = {
-      tagName: 'style',
-      closeTag: true,
-      attributes: {
-        type: 'text/css'
-      }
-    };
-  }
-
-  if (assetUrl) {
-    // Strip public URL prefix from asset URL to get Webpack asset name
-    var publicUrlPrefix = compilation.outputOptions.publicPath || '';
-    // if filename is in subfolder, assetUrl should be prepended folder path
-    if (path.basename(filename) !== filename) {
-      assetUrl = path.dirname(filename) + '/' + assetUrl;
     }
-    var assetName = path.posix.relative(publicUrlPrefix, assetUrl);
-    var asset = getAssetByName(compilation.assets, assetName);
-    var updatedSource = this.resolveSourceMaps(compilation, assetName, asset);
-    tag.innerHTML = (tag.tagName === 'script') ? updatedSource.replace(/(<)(\/script>)/g, '\\x3C$2') : updatedSource;
+    // inline css
+    else if (tag.tagName === 'link' && regex.test(tag.attributes.href)) {
+        assetUrl = tag.attributes.href;
+        tag = {
+            tagName: 'style',
+            closeTag: true,
+            attributes: {
+                type: 'text/css'
+            }
+        };
+    }
 
-    // delete resource
-    delete compilation.assets[assetName];
-  }
+    if (assetUrl) {
+        // Strip public URL prefix from asset URL to get Webpack asset name
+        const publicUrlPrefix = compilation.outputOptions.publicPath || '';
+        // if filename is in subfolder, assetUrl should be prepended folder path
+        if (path.basename(filename) !== filename) {
+            assetUrl = path.dirname(filename) + '/' + assetUrl;
+        }
+        const assetName = path.posix.relative(publicUrlPrefix, assetUrl);
+        const asset = getAssetByName(compilation.assets, assetName);
+        const updatedSource = this.resolveSourceMaps(compilation, assetName, asset);
+        tag.innerHTML = (tag.tagName === 'script') ? updatedSource.replace(/(<)(\/script>)/g, '\\x3C$2') : updatedSource;
 
-  return tag;
+        // delete resource
+        delete compilation.assets[assetName];
+    }
+
+    return tag;
 };
 
-function getAssetByName (assests, assetName) {
-  for (var key in assests) {
-    if (assests.hasOwnProperty(key)) {
-      var processedKey = path.posix.relative('', key);
-      if (processedKey === assetName) {
-        return assests[key];
-      }
+function getAssetByName(assests, assetName) {
+    for (let key in assests) {
+        if (assests.hasOwnProperty(key)) {
+            const processedKey = path.posix.relative('', key);
+            if (processedKey === assetName) {
+                return assests[key];
+            }
+        }
     }
-  }
 }
 
 module.exports = HtmlWebpackInlineSourcePlugin;
